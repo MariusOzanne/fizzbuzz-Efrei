dda3bf3fa919c4869d200865cd95c359bd862945
MariusOzanne <ozanne.marius@gmail.com>
2025-02-07 16:03:11 +0100

    docker image

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 def fizzbuzz(num):
     if num%3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
         return "fizzbuzz"
     elif num%3 == 0:
+        print("fizz")
         return "fizz"
     elif num % 5 == 0:
+        print("buzz")
         return "buzz"
     else:
+        print(str(num))
         return str(num)
-    
-def main():
+
+if __name__ == '__main__':
     for i in range(1, 101):
-        if i%3 == 0 and i % 5 == 0:
-            return "fizzbuzz"
-        elif i%3 == 0:
-            return "fizz"
-        elif i % 5 == 0:
-            return "buzz"
-        else:
-            return str(i)
+       fizzbuzz(i)
